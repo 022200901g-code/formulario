0efdae472e65599cfc2a5e0721e238950b413c96
edgar hugo <022200901g@uandina.edu.pe>
2025-09-04 17:19:55 -0500

    V2

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,49 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 
-class LoginData{
+class LoginData {
   String username = '';
   String password = '';
 }
 
 void main() => runApp(const MainApp());
-  
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key}): super(key: key);
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+
   @override
   _MyApp createState() => _MyApp();
+}
 
-
-class _MyApp extends State{
+class _MyApp extends State<MainApp> {
   LoginData _loginData = LoginData();
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
-  Widget @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Codigo Flutter',
-      home: Scaffold{
-        AppBar: AppBar(title: Text('codigo flutter')),
-        Body: Container(
+      home: Scaffold(
+        appBar: AppBar(title: Text('codigo flutter')),
+        body: Container(
           padding: EdgeInsets.all(50.0),
-          child: Formulario(formKey:_formKey,loginData:_loginData),
-        )
-      },
+          child: Formulario(formKey: _formKey, loginData: _loginData),
+        ),
+      ),
     );
   }
 }
 
-class Formularion extends StatelessWidget{
+class Formulario extends StatelessWidget {
   const Formulario({
-    key key,
-    @required GlobalKey<FormState> formKey,
-    @required LoginData loginData,
-  })  :_formKey = formKey,
-       _loginData = loginData,
-       super(key:key);
+    Key? key,
+    required GlobalKey<FormState> formKey,
+    required LoginData loginData,
+  })  : _formKey = formKey,
+        _loginData = loginData,
+        super(key: key);
 
   final GlobalKey<FormState> _formKey;
-  final LoginData _loginData;    
+  final LoginData _loginData;
 
   @override
   Widget build(BuildContext context) {
@@ -55,50 +54,48 @@ class Formularion extends StatelessWidget{
         children: [
           TextFormField(
             keyboardType: TextInputType.emailAddress,
-            validator: (String inValue){
-              if(inValue.length == 0){
+            validator: (String? inValue) {
+              if (inValue == null || inValue.isEmpty) {
                 return 'Introduce nombre de usuario';
               }
               return null;
             },
-            onSaved: (String inValue){
-              _loginData.username = inValue;
+            onSaved: (String? inValue) {
+              _loginData.username = inValue ?? '';
             },
             decoration: InputDecoration(
-              hintText: 'none@none.com', labelText: 'Usuario (eMail)'
+              hintText: 'none@none.com',
+              labelText: 'Usuario (eMail)',
             ),
+          ),
           TextFormField(
             obscureText: true,
-            validator: (String inValue){
-              if(inValue.length < 10){
+            validator: (String? inValue) {
+              if (inValue == null || inValue.length < 10) {
                 return 'la contraseña debe tener al menos 10 caracteres';
               }
               return null;
             },
-            onSaved: (String inValue){
-              _loginData.password = inValue;
+            onSaved: (String? inValue) {
+              _loginData.password = inValue ?? '';
             },
             decoration: InputDecoration(
-              hintText: 'contraseña', labelText: 'Contraseña'
+              hintText: 'contraseña',
+              labelText: 'Contraseña',
             ),
-          RaisedButton(
+          ),
+          ElevatedButton(
             child: Text('acceso'),
-            onPressed:(){
-              if(_formKey.currentState.validate()){
-                _formKey.currentState.save();
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
                 print('Usuario: ${_loginData.username}');
                 print('Contraseña: ${_loginData.password}');
               }
-            }
-          )
-          )
-
-
-          )
+            },
+          ),
         ],
       ),
-      )
+    );
   }
-}
- 
 }
